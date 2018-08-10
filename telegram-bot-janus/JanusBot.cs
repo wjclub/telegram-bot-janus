2bf013d906a914ecfcf649b7b98b11cded8d7c13
browny99 <browny@wjclub.tk>
2018-08-10 21:49:35 +0200

    Added database credential constructor

---
+++ telegram-bot-janus/JanusBot.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace telegram_bot_janus {
+	internal class JanusBot {
+		public JanusBot(string databaseUser, string databasePassword, string databaseName) {
+
+		}
+	}
+}
--- telegram-bot-janus/Program.cs
+++ telegram-bot-janus/Program.cs
@@ -7,6 +7,9 @@ using System.Threading.Tasks;
 namespace telegram_bot_janus {
 	class Program {
 		static void Main(string[] args) {
+			if (args.Length == 3) {
+				JanusBot janusBot = new JanusBot(databaseUser: args[0], databasePassword: args[1], databaseName: args[2]);
+			}
 		}
 	}
 }
