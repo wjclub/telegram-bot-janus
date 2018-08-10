bf4f3cd7006076617c69df729bf0c579c49537f2
browny99 <browny@wjclub.tk>
2018-08-10 23:34:47 +0200

    Added tg bot lib and mysql lib and some additional helper classes

---
+++ telegram-bot-janus/DBHandler.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MySql.Data;
+
+namespace telegram_bot_janus {
+	internal class DBHandler {
+		public DBHandler(string databaseUser, string databasePassword, string databaseName) {
+			
+		}
+
+		internal string[] GetApikeys() => new string[]{ "" };
+	}
+}
--- telegram-bot-janus/JanusBot.cs
+++ telegram-bot-janus/JanusBot.cs
@@ -3,11 +3,27 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Telegram.Bot;
 
 namespace telegram_bot_janus {
 	internal class JanusBot {
 		public JanusBot(string databaseUser, string databasePassword, string databaseName) {
+			dBHandler = new DBHandler(databaseUser: databaseUser, databasePassword: databasePassword, databaseName: databaseName);
+			string[] apikeys = dBHandler.GetApikeys();
+			foreach (string apikey in apikeys) {
+				clients.Add(new TelegramBotClient(apikey), );
+			}
+		}
+		DBHandler dBHandler;
 
+		Dictionary<TelegramBotClient, int> clients = new Dictionary<TelegramBotClient, int>();
+
+		internal async Task Run() {
+			while (true) {
+				foreach (TelegramBotClient client in clients) {
+					client.GetUpdatesAsync();
+				}
+			}
 		}
 	}
 }
